56b0320e6ee8150510f9d46e9410adbb6580d922
ManishAnand05 <manishanand557@gmail.com>
2026-01-25 12:27:26 +0530

    path changes

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+<<<<<<< HEAD
 from app.schemas.user import UserCreate, UserLogin
+=======
+from app.schemas.user import UserCreate
+>>>>>>> 1826cb3 (path changes)
 from app.models.user import User
 from app.core.database import get_db
 
